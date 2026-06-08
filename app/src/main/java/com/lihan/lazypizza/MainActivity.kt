4aa9e8d73184bc00993fa2507d10a064bf60071e
LiHan <encorex32268@gmail.com>
2026-06-08 23:04:48 +0800

    Upload Products and Toppings , Add Internet Permission

--- app/src/main/java/com/lihan/lazypizza/MainActivity.kt
+++ app/src/main/java/com/lihan/lazypizza/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.lihan.lazypizza
 
+import android.content.Context
 import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -13,38 +14,51 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
+import com.google.firebase.firestore.FirebaseFirestore
 import com.lihan.lazypizza.core.presentation.ui.theme.LazyPizzaTheme
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import java.io.BufferedReader
+import java.io.File
+import java.io.InputStreamReader
+import kotlin.text.set
+
+
+
+//@Serializable
+//data class Product(
+//    val id: String = "",
+//    val name: String = "",
+//    val price: Double = 0.0,
+//    val category: String = "",
+//    val imageUrl: String = "",
+//    val ingredients: List<String> = listOf()
+//)
+//
+//
+//@Serializable
+//data class ToppingDto(
+//    val id: String = "",
+//    val name: String = "",
+//    val price: Double = 0.0,
+//    val imageUrl: String = "",
+//)
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             LazyPizzaTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier,
-        color = MaterialTheme.colorScheme.primary
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LazyPizzaTheme {
-        Greeting("Android")
     }
 }
