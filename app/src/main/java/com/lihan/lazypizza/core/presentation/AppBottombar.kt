35888580ababce26a80e3f96f851c2c290e56ae6
LiHanChen <lihan.lh.chen@ptc-nec.com.tw>
2026-06-12 16:41:29 +0800

    feat(navigation): implement AppBottomBar and AppNavigationRoot with conditional visibility

--- app/src/main/java/com/lihan/lazypizza/core/presentation/AppBottombar.kt
+++ app/src/main/java/com/lihan/lazypizza/core/presentation/AppBottombar.kt
@@ -1,2 +1,107 @@
 package com.lihan.lazypizza.core.presentation
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.lihan.lazypizza.core.presentation.ui.theme.LazyPizzaTheme
+import com.lihan.lazypizza.core.presentation.ui.theme.primary8
+import com.lihan.lazypizza.core.presentation.ui.theme.surfaceHigher
+import com.lihan.lazypizza.core.presentation.ui.theme.title4
+
+@Composable
+fun AppBottomBar(
+    currentRoute: Route,
+    onItemClick: (Route) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    NavigationBar(
+        modifier = modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp)),
+        containerColor = MaterialTheme.colorScheme.surfaceHigher,
+        contentColor = Color.Transparent
+    ) {
+        bottomBarItems.forEach { bottomBarItem ->
+            val isSelected = bottomBarItem.route == currentRoute
+            NavigationBarItem(
+                selected = false,
+                colors = NavigationBarItemDefaults.colors(
+                    unselectedIconColor = if (isSelected) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.secondary,
+                    unselectedTextColor = if (isSelected) MaterialTheme.colorScheme.onBackground else MaterialTheme.colorScheme.secondary,
+                ),
+                alwaysShowLabel = true,
+                onClick = {
+                    onItemClick(bottomBarItem.route)
+                },
+                icon = {
+                    BadgedBox(
+                        badge = {
+                            if (bottomBarItem.badge > 0){
+                                Text(
+                                    text = bottomBarItem.badge.toString(),
+                                    style = MaterialTheme.typography.title4.copy(
+                                        color = MaterialTheme.colorScheme.onSecondary
+                                    ),
+                                )
+                            }
+                        }
+                    ) {
+                        Icon(
+                            imageVector = bottomBarItem.icon,
+                            contentDescription = null
+                        )
+                        Box(
+                            modifier = Modifier
+                                .size(28.dp)
+                                .clip(CircleShape)
+                                .background(
+                                    if (isSelected){
+                                        MaterialTheme.colorScheme.primary8
+                                    }else{
+                                        Color.Transparent
+                                    }
+                                )
+                        )
+
+                    }
+                },
+                label = {
+                    Text(
+                        text = bottomBarItem.route.toRouteName(),
+                        style = MaterialTheme.typography.title4
+                    )
+                }
+            )
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+private fun AppBottomBarPreview() {
+    LazyPizzaTheme {
+        AppBottomBar(
+            currentRoute = Route.Menu,
+            onItemClick = {}
+        )
+    }
+}
--- app/src/main/java/com/lihan/lazypizza/core/presentation/AppNavigationRoot.kt
+++ app/src/main/java/com/lihan/lazypizza/core/presentation/AppNavigationRoot.kt
@@ -1,14 +1,26 @@
 package com.lihan.lazypizza.core.presentation
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.LocalRippleConfiguration
+import androidx.compose.material3.RippleConfiguration
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavController
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavDestination.Companion.hasRoute
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import com.lihan.lazypizza.core.domain.Route
+import androidx.navigation.navigation
+import androidx.navigation.toRoute
 import com.lihan.lazypizza.menu.presentation.MenuRoot
 import com.lihan.lazypizza.menu.presentation.product_detail.ProductDetailRoot
 
@@ -17,26 +29,96 @@ fun AppNavigationRoot(
     modifier: Modifier = Modifier,
     navController: NavHostController = rememberNavController()
 ) {
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
 
-    NavHost(
+    val showBottomBar = currentDestination?.run {
+                hasRoute<Route.Menu>() ||
+                hasRoute<Route.Cart>() ||
+                hasRoute<Route.History>() ||
+                hasRoute<Route.MenuGraph>() ||
+                hasRoute<Route.CartGraph>() ||
+                hasRoute<Route.HistoryGraph>()
+    } ?: false
+
+
+    Scaffold(
         modifier = modifier,
-        navController = navController,
-        startDestination = Route.Menu
-    ){
-        composable<Route.Menu>{
-            MenuRoot(
-                onNavigateToDetail = { id ->
-                    navController.navigate(Route.ProductDetail(id))
-                }
-            )
-        }
+        bottomBar = {
+            if (showBottomBar){
 
-        composable<Route.ProductDetail>{
-            ProductDetailRoot(
-                onBack = {
-                    navController.navigateUp()
+                val currentRoute = bottomBarItems.find {
+                    currentDestination.hasRoute(it.route::class)
+                }?.route ?: Route.Menu
+
+                AppBottomBar(
+                    currentRoute = currentRoute,
+                    onItemClick = { route ->
+                        navController.navigate(route){
+                            popUpTo(Route.Menu) {
+                                saveState = true
+                            }
+                            launchSingleTop = true
+                            restoreState = true
+                        }
+                    }
+                )
+            }
+        }
+    ) { padding ->
+        NavHost(
+            modifier = Modifier.fillMaxSize().padding(padding),
+            navController = navController,
+            startDestination = Route.MenuGraph
+        ){
+            navigation<Route.MenuGraph>(
+                startDestination = Route.Menu
+            ){
+                composable<Route.Menu>{
+                    MenuRoot(
+                        onNavigateToDetail = { id ->
+                            navController.navigate(Route.ProductDetail(id))
+                        }
+                    )
                 }
-            )
+                composable<Route.ProductDetail>{
+                    ProductDetailRoot(
+                        onBack = {
+                            navController.navigateUp()
+                        }
+                    )
+                }
+            }
+
+            navigation<Route.CartGraph>(
+                startDestination = Route.Cart
+            ){
+                composable<Route.Cart>{
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ){
+                        Text(
+                            text = "Cart"
+                        )
+                    }
+                }
+            }
+
+            navigation<Route.HistoryGraph>(
+                startDestination = Route.History
+            ){
+                composable<Route.History>{
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ){
+                        Text(
+                            text = "History"
+                        )
+                    }
+                }
+            }
         }
 
     }
